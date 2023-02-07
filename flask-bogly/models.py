cc8d0e2821e4e47f015741c905e575590aac6aea
0UTC4STD <dylancourte@yahoo.com>
2023-02-07 12:49:23 -0500

    Update models.py

--- flask-bogly/models.py
+++ flask-bogly/models.py
@@ -13,6 +13,19 @@ class User(db.Model):
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
 
+class Post(db.Model):
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
 
 def connect_db(app):
 
